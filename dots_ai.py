4cd78dab5688fe705bc8d0c94dfc388143ca9675
brthhule <brennenle@yahoo.com>
2025-03-01 11:35:50 -0500

    started working on decision tree

---
+++ dots_ai.py
@@ -0,0 +1,24 @@
+class lot
+
+
+
+
+def decision_structure(identity):
+    # What do you identify as?
+    if (identity == "Student"):
+        student_func()
+    elif (identity == "Faculty/Staff"):
+        faculty_staff_func()
+    elif (identity == "Visitor"):
+        visitor_func() 
+
+
+
+
+
+
+def visitor_func():
+
+def faculty_staff_func():
+
+def student_func():
